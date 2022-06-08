941e3f2aaaa9d70e5ea49be3a440fade08766c01
larrissalira <larrissalira@outlook.com>
2022-06-08 18:19:42 -0300

    correções no código

--- js/adalberto.js
+++ js/adalberto.js
@@ -6,7 +6,7 @@ escolhaAdalberto.addEventListener('click', function (primeiroEscolha) {
         primeiraEscolha = prompt("Resposta inválida, escolha entre 1 ou 2")
     } if (primeiraEscolha == 1) {
         alert("Adalberto levou a mulher para o hospital, passou o dia inteiro lá e perdeu a reunião. No dia seguinte quando Adalberto chegou ao trabalho, chamou o chefe para conversar e soube que não havia mais chances de ser promovido, pois a vaga foi preenchida na reunião de ontem. \nAdalberto se LAXCOU!")
-        return location = "../html/gameover.html"
+        return location = "../html/perdeu.html"
     } else {
         faseDois()
     }
@@ -18,7 +18,7 @@ function faseDois(adalbertoSegunda) {
         adalbertoSegunda = prompt("Resposta inválida, escolha entre 1 ou 2")
     } if (adalbertoSegunda == 1) {
         alert("Adalberto chamou o chefe em um momento em que ele não estava falando na reunião, e explicou o ocorrido, justificando o atraso. \nAo terminar a reunião o chefe o chama no escritório, dando a ele a promoção! \nPois o chefe valoriza o relacionamento dele como os subordinados e valoriza a conduta tomada. \nAdalberto volta ao hospital e conta para a companheira que foi promovido.")
-        return location = "../html/venceu.html"
+        return location = "../html/ganhou.html"
     } else {
         faseTres()
     }
@@ -30,9 +30,9 @@ function faseTres(adalbertoSegunda) {
         adalbertoTerceira = prompt("Resposta inválida, escolha entre 1 ou 2")
     } if (adalbertoTerceira == 1) {
         alert("Após a reunião, ele é chamado na sala do chefe e recebe a notícia de que está sendo demitido. \nAdalberto não se comunicou corretamente com a liderança, e ninguém sabia da sua situação. \nEle foi demitido e se LAXCOU!")
-        return location = "../html/gameover.html"
+        return location = "../html/perdeu.html"
     } if (adalbertoTerceira == 2) {
         alert("Adalberto é chamado na sala do chefe que se sentiu ofendido com a conduta em reunião, \Ne exige que para não o demitir que Adalberto demita 5 funcionários de sua equipe. \NAdalberto demite os 5 funcionários, não consegue a promoção e agora tem que entregar o mesmo resultado com 5 funcionários a menos, \nele e os empregados se LAXCARAM!")
-        return location = "../html/gameover.html"
+        return location = "../html/perdeu.html"
     }
 }
